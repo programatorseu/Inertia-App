b1077c37eafa5a4c05daa0299abfb9c038905f70
programators <programatorseu@gmail.com>
2022-10-25 15:08:01 +0200

    5 preserve Scroll position

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,10 @@ Route::get('/', function () {
 });
 
 Route::get('/users', function () {
-    sleep(2);
-    return Inertia::render('Users');
+
+    return Inertia::render('Users',[
+        'time' => now()->toTimeString()
+    ]);
 });
 Route::get('/settings', function () {
     return Inertia::render('Settings');
