b1cb3eed0302eb760434b0adb4b517044e3dbaa5
programators <programatorseu@gmail.com>
2022-10-26 10:48:14 +0200

    16 search - filter

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,6 +1,10 @@
 <template>
     <Head title="Users"/>
-    <h1 class="text-4xl font-bold">Users</h1>
+    <div class="flex justify-between mb-4">
+        <h1 class="text-4xl font-bold">Users</h1>
+        <input v-model="search" type="text" placeholder="Search..." class="border px-2 rounded-lg">
+    </div>
+
     <div class="flex flex-col">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
@@ -33,6 +37,20 @@
   <Pagination :links="users.links" class="mt-6" />
 </template>
     <script setup>
-    import Pagination from "../Shared/Pagination";
-        defineProps({users:Object})
+        import Pagination from "../Shared/Pagination";
+        import {ref, watch} from "vue";
+        import { Inertia } from "@inertiajs/inertia";
+        
+       let props = defineProps({
+            users:Object,
+            filters: Object
+        });
+        
+        let search =  ref(props.filters.search);
+        watch(search, value => {
+            Inertia.get('/users', {search: value}, {
+                preserveState:true,
+                replace: true
+            });
+        });
     </script>
